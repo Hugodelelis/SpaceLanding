28d60a141b9a7df2f298fd3b4b8018afbfac4a44
Hugo de Lelis <hugodelelis00@gmail.com>
2024-11-13 17:07:11 -0300

    chore(routes) 🔧: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,27 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { DestinationComponent } from './pages/destination/destination.component';
+import { CrewComponent } from './pages/crew/crew.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+
+  {
+    path: '',
+    component: DestinationComponent
+  },
+
+  {
+    path: '',
+    component: CrewComponent
+  },
+
+  {
+    path: '',
+    component: TechnologyComponent
   }
 ];
